836fe63c22ec4d56f404d3f6e2be158a690ef95d
Otabek Abdiraimov <otabekabdiraimovv@gmail.com>
2024-11-25 11:50:49 +0500

    deployment

--- posts/admin.py
+++ posts/admin.py
@@ -1,14 +1,54 @@
+import markdown
 from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, User
 from django.db import models
 from django.forms import widgets
 from django.utils.safestring import mark_safe
-import markdown
+from import_export.admin import ImportExportModelAdmin
+from unfold.admin import ModelAdmin
+from unfold.contrib.import_export.forms import ExportForm, ImportForm
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+
 from .models import Post
-from django.contrib.admin import ModelAdmin
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+
+    list_display = ("name", "id")
+
+    search_fields = ("name",)
+
+    fields = ("name", "permissions")
+
+    list_filter = ("permissions",)
+
+    unfold_settings = {
+        "layout": "tabs",
+    }
+
+    def save_model(self, request, obj, form, change):
+        # Add custom logic before saving the group
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(Post)
-class PostAdmin(ModelAdmin):
+class PostAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
     list_display = ["title", "slug", "created_at", "published", "short_desc"]
     search_fields = ["title", "content"]
     list_filter = ["published", "created_at"]
@@ -67,3 +107,6 @@ class PostAdmin(ModelAdmin):
             },
         ),
     )
+
+    class Media:
+        css = {"all": ("css/custom_admin.css",)}
